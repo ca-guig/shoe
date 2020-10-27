180f9f8748001642d172cca85dd1ef53b32a9c24
Guillaume Girard <guillaume.girard@ingeno.ca>
2020-10-27 17:39:57 -0400

    Set up project

---
+++ src/main/java/ca/guig/shoe/Application.java
@@ -0,0 +1,6 @@
+package ca.guig.shoe;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Application {}
---
+++ src/main/java/ca/guig/shoe/Launcher.java
@@ -0,0 +1,12 @@
+package ca.guig.shoe;
+
+import org.springframework.boot.SpringApplication;
+
+public final class Launcher {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    private Launcher() {}
+}
